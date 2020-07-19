4f0301a2706e3e7fd52a943b02de43ec465010cd
richard-mcghee <richardjmcghee@yahoo.com>
2020-07-19 05:20:53 -0400

    finished the header tasks and applied it to the dom, initial commit

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let newHeaderDiv = document.createElement('div')
+    let newDateSpan = document.createElement('span')
+    let newTempSpan = document.createElement('span')
+    let newH1 = document.createElement('h1')
+
+    newHeaderDiv.classList.add('header')
+    newDateSpan.classList.add('date')
+    newTempSpan.classList.add('temp')
+    
+    newDateSpan.textContent = 'MARCH 28, 2020'
+    newTempSpan.textContent = '98°'
+    newH1.textContent = 'Lambda Times'
+
+    newHeaderDiv.appendChild(newDateSpan)
+    newHeaderDiv.appendChild(newH1)
+    newHeaderDiv.appendChild(newTempSpan)
+
+    return newHeaderDiv
+}
+
+let headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(Header())
